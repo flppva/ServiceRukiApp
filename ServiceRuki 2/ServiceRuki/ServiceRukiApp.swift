154da0f5c69e89d8f93fe347cf6b56a563130cef
flppva <eleonorandreevnaa@gmail.com>
2024-10-01 15:13:58 +0300

    Add files via upload

---
+++ ServiceRuki 2/ServiceRuki/ServiceRukiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceRukiApp.swift
+//  ServiceRuki
+//
+//  Created by Эльвира Филиппова on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ServiceRukiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() 
+        }
+    }
+}
